0852d78783fa4b5a5d8d7f5eca8dda5f44f8c55a
Filipe Pires <filipepires@gmail.com>
2025-05-12 21:41:13 +0000

    Não está consegui do acessar a câmera,  nem licicitando acesso

--- next.config.ts
+++ next.config.ts
@@ -59,7 +59,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: 'Permissions-Policy',
-            value: "camera=(), microphone=(), geolocation=()", 
+            value: "camera=(self), microphone=(), geolocation=()", 
           },
         ],
       },
